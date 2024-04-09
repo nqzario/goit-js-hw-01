98a9900199d3ac3375035c9b0a4fc5aa422f075b
nqzario <159358436+elizabetlisa@users.noreply.github.com>
2024-04-09 23:36:34 +0300

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+    const message = Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2 + Number.parseInt(content); 
+    return message;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
